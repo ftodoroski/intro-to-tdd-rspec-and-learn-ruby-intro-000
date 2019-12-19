9e0bd5862a6aad677f8a4a96f7d058e841452c6a
Filip Todoroski <ftodoroski@ymail.com>
2019-12-19 17:36:25 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,6 +1,6 @@
-require_relative '../current_age_for_birth_year.rb'
+require_relative '../current_age_for_birth_year.rb' # go back one directory and load current_age_for_birth_year.rb
 
-describe "current_age_for_birth_year method" do
+describe "current_age_for_birth_year method" do # Defines an Example Group name for a collection of test
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(1984)
 
